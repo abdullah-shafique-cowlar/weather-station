0808ec22fe3934693d2f851bf3cc36d5ae6c13f4
abdullah <abdullah.shafique.cowlar@gmail.com>
2023-07-05 14:50:15 +0500

    Refac(app.js): app.js refactored

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,9 @@
 var express = require("express");
 var logger = require("morgan");
-require("dotenv").config();
-const helmet = require("helmet");
 const cors = require("cors");
 const influx_client = require("./config/db.utils").getClient();
 const client = require('./config/mqtt.utils')
+require("dotenv").config();
 
 client.connect();
 mqttClient = client.getClient()
@@ -15,24 +14,20 @@ var apiRouterV1 = require("./routes/v1/api");
 var app = express();
 
 const corsOptions = {
-  //multiple origins
   origin: "http://localhost:5173",
   methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE", "PATCH"],
   optionsSuccessStatus: 200,
 };
+
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ limit: "100mb" }));
-app.use(helmet()); // Helmet helps you secure your Express apps by setting various HTTP headers.
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// on receive message event, log the message to the console
+// On Message receive event, write to InfluxDB
 mqttClient.on("message", async (topic, message, packet) => {
-  //   console.log(packet, packet.payload.toString());
   if (packet.retain === true) {
-    // console.debug("Received retained message. Skipping write to the database.");
+    console.debug("Received retained message. Skipping write to the database.");
     return;
   }
   if (topic === client.topicName) {
@@ -51,9 +46,9 @@ mqttClient.on("message", async (topic, message, packet) => {
       };
 
       await influx_client.writePoints([dataPoint]);
-      console.log("[+] Point Written: ", dataPoint);
+      console.debug("[+] Point Written: ", dataPoint);
     } catch (error) {
-        console.log("[-] Error: ", error);
+        console.debug("[-] Error: ", error);
     }
   }
 });
