5a671e8143cfd76e200055d4e24eccbd37dd3e39
abdullah <abdullah.shafique.cowlar@gmail.com>
2023-07-05 18:55:57 +0500

    refac(config): Refactored configjs file

--- backend/config/env.config.js
+++ backend/config/env.config.js
@@ -3,21 +3,21 @@ require("dotenv").config();
 const config = {
     development: {
         port: 3000,
-        secret: "secret",
+        secret: process.env.SECRET || "secret",
         influxdb: {
             measurement: "weather_data"
         }
     },
     test: {
         port: 3000,
-        secret: "secret",
+        secret: process.env.SECRET || "secret",
         influxdb: {
             measurement: "test_measurement"
         }
     },
     production: {
         port: 3000,
-        secret: "gahgj65677ghd678dgs7d8tsgd7809965g"
+        secret: process.env.SECRET || "gahgj65677ghd678dgs7d8tsgd7809965g"
     }
 }
 
