0ac9af2dc4cff9861196b78ddc03a82cb89c1185
abdullah <abdullah.shafique.cowlar@gmail.com>
2023-07-05 17:53:03 +0500

    Refac(frontend): Init refactored and clean code for frontend dev.

--- frontend/weatherapp/src/views/HomeView.vue
+++ frontend/weatherapp/src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Hello</h1>
+  </div>
 </template>
+
+<script setup>
+
+</script>
