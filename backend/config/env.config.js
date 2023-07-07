342c5578a824fe63bbb99b501c5eabf515ccb5aa
abdullah <abdullah.shafique.cowlar@gmail.com>
2023-07-07 18:11:56 +0500

    Add(env): Added env vars for influxdb 2

--- backend/config/env.config.js
+++ backend/config/env.config.js
@@ -7,10 +7,15 @@ const config = {
         influxdb: {
             INFLUXDB_USERNAME: process.env.INFLUXDB_USERNAME || "root",
             INFLUXDB_PASS: process.env.INFLUXDB_PASS || "root",
-            INFLUXDB_HOST: process.env.INFLUXDB_HOST || "localhost",
+            INFLUXDB_HOST: process.env.INFLUXDB_HOST || "http://localhost",
             measurement: process.env.INFLUXDB_DATABASE_DEV || "weather_data",
             INFLUX_DATABASE: process.env.INFLUX_DATABASE_DEV || "weather_dev",
-            TOPIC: process.env.TOPIC_DEV || "Weather_Data"
+            TOPIC: process.env.TOPIC_DEV || "Weather_Data",
+            INFLUX_PORT: process.env.INFLUXDB_PORT || 8086,
+            INFLUX_ORG: process.env.INFLUX_ORG || "Cowlar",
+            INFLUX_BUCKET: process.env.INFLUX_BUCKET || "weather_data",
+            INFLUX_TOKEN: process.env.INFLUX_TOKEN,
+            INFLUX_MEASUREMENT: process.env.INFLUX_MEASUREMENT || "weather_measurement"
         }
     },
     test: {
