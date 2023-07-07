e71ada3f1378c2b03d5f97dd4bb8871da0c1257d
abdullah <abdullah.shafique.cowlar@gmail.com>
2023-07-07 11:21:15 +0500

    Add(user schema): Added update schema

--- backend/schemas/user.schema.js
+++ backend/schemas/user.schema.js
@@ -11,6 +11,11 @@ const userSchema = {
     email: Joi.string().email().required(),
     password: Joi.string().alphanum().min(6).max(15).required()
   }),
+  update: Joi.object().keys({
+    user_name: Joi.string().alphanum().min(5).max(15).optional(),
+    email: Joi.string().email().optional(),
+    password: Joi.string().alphanum().min(6).max(15).optional()
+  })
 };
 
 module.exports = userSchema;
